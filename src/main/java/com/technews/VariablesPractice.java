5a3ecf5dabf18870dab4234a468df506d3afc1f3
bob.w.perez <bob.w.perez@gmail.com>
2021-09-12 20:58:21 -0400

    setup database structure

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int numOne = 55;
+        double numTwo = 10.55;
+        String word = "meeow";
+        double sum = numOne + numTwo;
+        String concat = numOne + word;
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+}
